d8738a6b1409cee5f1af3d3910dda40a45668bd7
jgOhYeah <33143109+jgOhYeah@users.noreply.github.com>
2023-08-11 22:15:09 +1000

    Adding command line options and handling invalid dates (#1)
    * Added command line options to control actions
    
    * Added option to enable time correction, handling invalid dates
    
    * Autoformatted
    
    * Added support for writing to file, updated readme
    
    * Inverted -r, added error handling for kernal driver disable, added windows readme part
    
    ---------
    
    Co-authored-by: Jotham Gates <jgat0002@student.monash.edu>

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Omron MIT Elite Plus HEM-7301-ITKE7
-Python script to extract measurements from an Omron MIT Elite Plus (HEM-7301-ITKE7) blood pressure monitor with USB support (0590:0028).
+Python script to extract measurements from an Omron MIT Elite Plus (`HEM-7301-ITKE7`) blood pressure monitor with USB support (`0590:0028`). This has also been tested to work with an Omron T9P (`HEM-759P-C1`) using the `HHX-CABLE-USB1`. This cable claims to work with `HJ-720IT`, `HEM-7080IT-E` and `HEM-790IT` models, although this has not been tested yet.
 
 ***
 
-Based on [https://usb2me.wordpress.com/2013/02/06/omron-mit-elite-plus-hem-7301-itke7](https://web.archive.org/web/20200704141615/https://usb2me.wordpress.com/2013/02/06/omron-mit-elite-plus-hem-7301-itke7/)
+Based on [https://usb2me.wordpress.com/2013/02/06/omron-mit-elite-plus-hem-7301-itke7](https://web.archive.org/web/20200704141615/https://usb2me.wordpress.com/2013/02/06/omron-mit-elite-plus-hem-7301-itke7/) ([direct link](https://usb2me.wordpress.com/2013/02/06/omron-mit-elite-plus-hem-7301-itke7)).
 
 > _This upper arm blood pressure monitor has memory for last 90 measurements. It records date, time, systolic/diastolic pressure and pulse. It has somewhat more complicated USB protocol using request/response packets. It has also support for wakeup request._
 
@@ -13,7 +13,49 @@ Based on [https://usb2me.wordpress.com/2013/02/06/omron-mit-elite-plus-hem-7301-
 git clone https://github.com/0x2b3bfa0/python-omron-mit-elite-plus.git
 cd python-omron-mit-elite-plus
 pip install -r requirements.txt
-python omron_mit_elite_plus.py
+```
+
+## Linux permissions
+By default, this script will need to be run as root to gain privileges to access the device. The `elevate` python module is used to request this automatically if running as the current user fails due to permissions issues. In the long term, it is recommended that udev rules be added to allow any (or selected) users to access the USB device so that running as root is not required. To do this:
+1. Copy the [`73-omron-bpm.rules`](./73-omron-bpm.rules) file to `/etc/udev/rules.d/73-omron-bpm.rules`.
+2. Run `sudo udevadm control --reload-rules && sudo udevadm trigger` as per [this post](https://unix.stackexchange.com/a/39371) to apply the new rules.
+
+## Windows driver installation
+This script uses the [PyUSB](https://github.com/pyusb/pyusb) library to communicate with the monitor. On Windows computers this usually needs a backend to be installed - see the [PyUSB readme](https://github.com/pyusb/pyusb) for more details.
+
+[Zadig](https://zadig.akeo.ie/) is one application that has been used with a little bit of trial and error by a family member to get this script / monitor to operate on Windows 10.
+
+## Usage
+This script is controlled through command line arguments. For example, to read all data on the monitor, write this to `output.csv` and then clear it, run
+```
+python omron_elite_plus.py -r -c -o output.csv
+```
+
+### Options
+```
+usage: omron_elite_plus.py [-h] [-r] [--correct-times] [-c] [-t]
+                           [-n] [-o OUTPUT]
+
+Tool for connecting to Omron branded blood pressure monitors
+
+options:
+  -h, --help            show this help message and exit
+  -r, --no-read         Do not read all data stored on the monitor.
+                        The default action if not provided is to
+                        read everything.
+  --correct-times       When reading, adds an offset from the
+                        computer's time to the monitor's time for
+                        each record to correct for the date and time
+                        on the monitor not being set correctly.
+  -c, --clear           Request that the monitor clear its internal
+                        memory after reading.
+  -t, --time            Get the current time from the monitor.
+  -n, --number          Get the number of records stored on the
+                        monitor.
+  -o OUTPUT, --output OUTPUT
+                        Write the results to the provided file
+                        instead of to the console.
+
 ```
 
 ## Alternatives
--- omron_elite_plus.py
+++ omron_elite_plus.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
+"""omron_elite_plus.py
+Script for connecting to Omron blood pressure monitors over USB.
 
+Originally based on
+https://usb2me.wordpress.com/2013/02/06/omron-mit-elite-plus-hem-7301-itke7
+
+Modified and added to by Helio Machado and Jotham Gates
+"""
 import usb
-
-from operator import xor
-from functools import reduce
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from elevate import elevate
+import argparse
+import errno
+import sys
 
-class ElitePlus():
+
+class BPMNotFoundError(Exception):
+    ...
+
+
+DATETIME_FORMAT = "%Y-%d-%m %H:%M:%S"
+
+
+class ElitePlus:
     """MIT Elite Plus HEM-7301-ITKE7 USB blood pressure meter 0590:0028."""
+
     @dataclass
     class Measurement:
         time: datetime
@@ -17,40 +32,52 @@ class ElitePlus():
         diastolic: int
         pulse: int
 
-    def __init__(self, vendor=0x0590, product=0x0028, timeout=0.5):
+    def __init__(self, vendor=0x0590, product=0x0028, timeout=4):
+        """Initialises the device connection.
+        Clearing may need a larger timeout compared to other options from
+        experience.
+        """
         self.vendor, self.product, self.timeout = vendor, product, timeout
         self.device = self.detect(self.vendor, self.product)
-        assert self.device
+        if not self.device:
+            raise BPMNotFoundError(
+                f"Blood pressure monitor with USB vendor id '{vendor:>04x}' and product id '{product:>04x}' not found."
+            )
         self.connect()
 
     @staticmethod
     def detect(vendor, product):
         """Detects the device."""
-        return usb.core.find(
-            idProduct=product,
-            idVendor=vendor
-            )
+        return usb.core.find(idProduct=product, idVendor=vendor)
 
     def connect(self):
         """Connects to the device."""
-        if self.device.is_kernel_driver_active(0):
-            self.device.detach_kernel_driver(0)
+        try:
+            if self.device.is_kernel_driver_active(0):
+                self.device.detach_kernel_driver(0)
+        except Exception as e:
+            print(
+                "Checking for kernal driver and detaching failed. Will continue on as if nothing happened."
+            )
+            print(f"    {e}")
+
         self.device.set_configuration()
-        #usb.util.claim_interface(device, None)
+        # usb.util.claim_interface(device, None)
 
         request_type = usb.util.build_request_type(
             recipient=usb.util.CTRL_RECIPIENT_INTERFACE,
             type=usb.util.CTRL_TYPE_CLASS,
-            direction=usb.util.CTRL_OUT
-            )
+            direction=usb.util.CTRL_OUT,
+        )
+
         self.device.ctrl_transfer(
             timeout=int(1000 * self.timeout),
             bmRequestType=request_type,
             data_or_wLength=(0, 0),
             wValue=0x300,
             bRequest=9,
-            wIndex=0
-            )
+            wIndex=0,
+        )
 
     def read(self):
         """Reads data from the device."""
@@ -59,10 +86,10 @@ class ElitePlus():
             chunk = bytes(self.device.read(0x81, 8, int(1000 * self.timeout)))
             if not chunk or chunk[0] not in range(1, 8):
                 return None
-            data += chunk[1:chunk[0]+1]
+            data += chunk[1 : chunk[0] + 1]
             if chunk[0] < 7:
                 break
-        #assert reduce(xor, data[2:], 0) == 0
+        # assert reduce(xor, data[2:], 0) == 0
         if data[0:2] == b"OK":
             return data[2:-1]
 
@@ -85,8 +112,8 @@ class ElitePlus():
         except usb.core.USBError:
             pass
         for _ in range(10):
-            self.write(7 * b'\x00')
-            self.write(7 * b'\x00')
+            self.write(7 * b"\x00")
+            self.write(7 * b"\x00")
             try:
                 response = self.read()
             except usb.core.USBError:
@@ -114,20 +141,33 @@ class ElitePlus():
         """Retrieves the number of measurements stored on the device memory."""
         return self.command(b"CNT00")[2]
 
-    def measurements(self):
-        """Retrieves all the measurements stored on the device memory."""
-        offset = (datetime.now() - self.clock()).total_seconds()
-        for index in range(self.count()):
-            record = self.command(b"MES\x00\x00", bytes([index]) * 2)
-            time = datetime(2000 + record[1], *record[2:7])
-            systolic, diastolic, pulse = record[9:12]
+    def measurements(self, correct_time: bool = True):
+        """Retrieves all the measurements stored on the device memory.
+        If correct_time is true, an offset of the computer's time minus the
+        monitor's time will be applied to each record to correct for the clock
+        not being set correctly."""
+        if correct_time:
+            # Calculate the time offset to apply if needed.
+            offset = (datetime.now() - self.clock()).total_seconds()
 
-            yield self.Measurement(
-                time + timedelta(seconds=offset),
-                systolic,
-                diastolic,
-                pulse
-                )
+        for index in range(self.count()):
+            # Get each record
+            record = self.command(b"MES\x00\x00", bytes([index]) * 2)
+            try:
+                time = datetime(2000 + record[1], *record[2:7])
+            except ValueError:
+                # Time isn't known / formatted correctly, leave out.
+                time = None
+
+            systolic, diastolic, pulse = record[9:12]
+            record = self.Measurement(time, systolic, diastolic, pulse)
+
+            if correct_time and time:
+                """Correct the time on the monitor with the computer's own
+                time offset if needed."""
+                record.time += timedelta(seconds=offset)
+
+            yield record
 
     def __len__(self):
         return self.count()
@@ -140,15 +180,128 @@ class ElitePlus():
         self.shutdown()
 
 
-if __name__ == "__main__":
-    elevate()  # Run as superuser
-    with ElitePlus() as meter:
-        for measurement in meter.measurements():
-            print(",".join([
-                str(measurement.time.replace(microsecond=0).isoformat().replace("T", " ")),
-                str(measurement.systolic),
-                str(measurement.diastolic),
-                str(measurement.pulse)
-                ]))
-        meter.clear()
+def main(settings: argparse.Namespace):
+    """Attempts to open the device and perform the required actions."""
+    try:
+        with ElitePlus() as meter:
+            if settings.time:
+                # Request the current time from the monitor.
+                print("Monitor's inbuilt clock")
+                print(meter.clock())
 
+            if settings.number:
+                # Request the number of records stored.
+                print("Number of records on device")
+                print(meter.count())
+
+            if not settings.no_read:
+                # Request all measurements from the monitor.
+                print("Date,Systolic,Diastolic,Pulse")
+                for measurement in meter.measurements(settings.correct_times):
+                    print(
+                        ",".join(
+                            [
+                                str(
+                                    measurement.time.strftime(DATETIME_FORMAT)
+                                    if measurement.time
+                                    else ""
+                                ),
+                                str(measurement.systolic),
+                                str(measurement.diastolic),
+                                str(measurement.pulse),
+                            ]
+                        )
+                    )
+
+            if settings.clear:
+                # Request that the monitor delete its internal data.
+                print("Requesting to clear internal data")
+                meter.clear()
+
+    except BPMNotFoundError as e:
+        print(e, file=sys.stderr)
+
+
+def parse_args() -> argparse.Namespace:
+    """Parser for command line arguments and help text."""
+    parser = argparse.ArgumentParser(
+        description="Tool for connecting to Omron branded blood pressure monitors"
+    )
+    parser.add_argument(
+        "-r",
+        "--no-read",
+        help="Do not read all data stored on the monitor. The default action if not provided is to read everything.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--correct-times",
+        help="When reading, adds an offset from the computer's time to the monitor's time for each record to correct for the date and time on the monitor not being set correctly.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-c",
+        "--clear",
+        help="Request that the monitor clear its internal memory after reading.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-t",
+        "--time",
+        help="Get the current time from the monitor.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-n",
+        "--number",
+        help="Get the number of records stored on the monitor.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Write the results to the provided file instead of to the console.",
+        type=str,
+    )
+
+    return parser.parse_args()
+
+
+def run_as_users():
+    """Attempts to run and connect as the current user. If this fails due to
+    permissions, then attempts to run as root."""
+    try:
+        # Attempt to run as the current user.
+        main(args)
+    except usb.core.USBError as e:
+        if e.errno == errno.EACCES:
+            # Running as the current user failed. Attempt to run as the root user.
+            print("Could not open the USB connection as:", file=sys.stderr)
+            print(f"    {e}", file=sys.stderr)
+            print("Will try to run as root.", file=sys.stderr)
+
+            # Only import now as not needed if permissions are otherwise ok
+            import elevate
+
+            elevate.elevate()
+
+            main(args)
+        else:
+            # Some other error that we don't know how to deal with.
+            raise e
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    # print(args)
+    if args.output:
+        # Output file provided.
+        try:
+            with open(args.output, "w") as out_file:
+                sys.stdout = out_file
+                run_as_users()
+        except OSError as e:
+            print(f"Could not open output file '{args.output}' as:", file=sys.stderr)
+            print(f"    {e}", file=sys.stderr)
+    else:
+        # No output file provided. Print to console
+        run_as_users()
